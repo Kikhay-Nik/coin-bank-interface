d0905f6303cbeceeeaf70d1f5da9c22c62148a73
Nikolay Kihay <nkihay@icloud.com>
2025-02-17 16:17:51 +0300

    initial commit

---
+++ frontend/src/index.js
@@ -0,0 +1,6 @@
+/* eslint-disable function-paren-newline */
+/* eslint-disable comma-dangle */
+/* eslint-disable implicit-arrow-linebreak */
+/* eslint-disable object-curly-newline */
+import './index.html';
+import './index.scss';
